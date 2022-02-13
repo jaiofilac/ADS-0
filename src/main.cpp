c0a9a3fd023f45f61eb7da9bd8e5c743d28694fd
Андрей Жаворонков <96244220+jaiofilac@users.noreply.github.com>
2022-02-13 21:15:10 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-// Copyright 2021 NNTU-CS
+#include <iostream>
 #include "alg.h"
-
-int main() {
-    int result = gcd(10, 15);
+int main()
+{
+    std::cout << gcd(10, 15) << std::endl;
     return 0;
 }
